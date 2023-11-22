0291f79cd2176e86e97df20aafe6017676376012
TaRa1999R <rezaietara78@gmail.com>
2023-11-23 01:06:30 +0330

    assignment26

--- 4.py
+++ 4.py
@@ -1,1 +1,9 @@
+
 import cv2
+import numpy
+
+img_black = numpy.zeros ((255 , 255))
+
+cv2.imshow ("result 4" , img_black)
+cv2.waitKey ()
+# cv2.imwrite ("4_output.jpg" , img_black)
--- 5.py
+++ 5.py
@@ -1,2 +1,17 @@
 
 import cv2
+import numpy
+
+img_white = numpy.ones ((300 , 300))
+
+for row in range (100 , 200) :
+    for col in range (135 , 165) :
+        img_white[row][col] = 0
+
+for row in range (70 , 100) :
+    for col in range (100 , 200) :
+        img_white[row][col] = 0
+
+cv2.imshow ("result 5" , img_white)
+cv2.waitKey ()
+cv2.imwrite ("5_output.jpg" , img_white)
